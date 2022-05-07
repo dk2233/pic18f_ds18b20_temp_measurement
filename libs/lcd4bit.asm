d602d6ab79650cf131daf3fa38a220f7ac6bb4bf
danielku <kucharski22daniel@gmail.com>
2022-05-07 14:31:06 +0200

    updated lcd4bit library

--- libs/lcd4bit.asm
+++ libs/lcd4bit.asm
@@ -7,6 +7,7 @@
 ;dla procesora 
 
     include "project_config.inc"
+    include "libs/lcd.inc"
 
 LCD_CODE     CODE
 wait
@@ -23,34 +24,29 @@ wait
 ;LCD
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
 ;przed wywolaniem funkcji umiesc w Wreg dane do wyslania
 send4bit
 send
 ;np PORTA bity 0-3  sa uzywane przez 4 bitowy ekran lcd
 ;najpierw wysylane sa 4bity gorne
-
-         movwf    dane_lcd
-         
+    movwf    dane_lcd
          ;bsf      port_lcd_e,enable
          
-      IF (polozenie_danych_lcd == 0) 
+    IF (polozenie_danych_lcd == 0) 
 ;jezli linia LCD jest na dolnych bitach portu przypisanego do LCD, wtedy to co jest
 ;wysylane musi byc najpierw starsze wyslane, wiec to co jest w rejestrze dane_lcd musi byc obrocone tak by starsze bity znalazly sie na miejscy mlodszych (dolnych)
          swapf    dane_lcd,f
 		 movlw	0xf0
 		 andwf	 latch_lcd,f 
                  ; i wyczyszczone aktualne bity w nim
-      ENDIF
+    ENDIF
             
-      IF (polozenie_danych_lcd == 1)        
-;jesli linia LCD jest podlaczona do gornych linii portu LCD nie na razie nie obracam
-            
+    IF (polozenie_danych_lcd == 1)        
+    ;jesli linia LCD jest podlaczona do gornych linii portu LCD nie na razie nie obracam
          movlw	0x0f
-	 andwf	 latch_lcd,f 
-      ENDIF
+         andwf	 latch_lcd,f 
+    ENDIF
 		  
-
          movlw    ktore_bity_uzywane_na_lcd
          andwf    dane_lcd,w
          addwf    latch_lcd,f
@@ -60,16 +56,14 @@ send
          nop
          bcf      latch_lcd_e,enable
 
-         
 
 ;jesli linia LCD jest podlaczona do gornych linii portu LCD, to wysylajac w drugiej kolejnosci mlodsze bity
 ;musze teraz obrcocic polowki bajtu, by dolne bity byly w miejscy starszych
          swapf    dane_lcd,f
 
-  IF (polozenie_danych_lcd == 0) 
+      IF (polozenie_danych_lcd == 0) 
 ;jezli linia LCD jest na dolnych bitach portu przypisanego do LCD, wtedy to co jest
 ;wysylane musi byc najpierw starsze wyslane, wiec to co jest w rejestrze dane_lcd musi byc obrocone tak by starsze bity znalazly sie na miejscy mlodszych (dolnych)
-        
 		 movlw	0xf0
 		 andwf	 latch_lcd,f 
       ENDIF
@@ -78,7 +72,7 @@ send
 ;jesli linia LCD jest podlaczona do gornych linii portu LCD nie na razie nie obracam
             
          movlw	0x0f
-	 andwf latch_lcd,f 
+         andwf latch_lcd,f 
       ENDIF
 
          movlw    ktore_bity_uzywane_na_lcd
@@ -89,8 +83,6 @@ send
          nop
          nop
          bcf      latch_lcd_e,enable
-            
-         
          return
 
          
@@ -98,12 +90,8 @@ send
          
          
          
-         
-         
 ;funkcja pisz?ca na ekranie
 write_lcd
-
-         
          bsf      latch_lcd_rs,rs
          
          call     send4bit
@@ -112,48 +100,16 @@ write_lcd
          return
 
 ;funkcja czyszcz?ca ekran
-
 cmd_off
-
-         
          bcf      port_lcd_rs,rs
          bcf      port_lcd_rw,rw
-         
-        return
-         
-
-
-;funkcja czysci cala linie
-; przed jej wywolaniem w W umi#1esc adres linii
-;po zakonczeniu z powrotem w niej umieszcza adres
-;stara wersja - dziala na cala linie
-
-clear_line_old
-         movwf    n
-         bcf      port_lcd_rs,rs
-         
-         movf     n,w
-         movwf    tmp_lcd
-         call     send
-         movlw    ile_znakow
-         movwf    tmp7
-         addlw    0xff
-         movwf    tmp
-clear_line_old2
-         call     check_busy4bit
-         movlw    " "
-         movwf    tmp_lcd
-         call     write_lcd
-         
-         decfsz   tmp7,f
-         goto     clear_line_old2
-         
-         call     cmd_off
-         movf     n,w
          return
          
+
+
+         
 clear_line
-;przed wywolaniem do n trzeba wrzucic ilosc kasowanych znakow
+;przed wywolaniem do n_lcd trzeba wrzucic ilosc kasowanych znakow
 ;a w W musi byc adres linii         
          movwf    dane_lcd
          call     send
@@ -164,7 +120,7 @@ clear_line_petla
          movwf    dane_lcd
          call     write_lcd
          
-         decfsz   n,f
+         decfsz   n_lcd,f
          goto     clear_line_petla
          
          call     cmd_off
@@ -201,10 +157,6 @@ check_busy4bit
          iorlw    ktore_bity_lcd_tris
          movwf    tris_lcd    
          
-        
-         
-         
-                  
          bsf      latch_lcd_rw,rw
          bcf      latch_lcd_rs,rs
 
@@ -292,7 +244,7 @@ lcd_init_hitachi
 	bcf	INTCON,T0IF
 ;wylaczam przerwania tmr0         
          movlw    3
-         movwf    tmp
+         movwf    n_lcd
         call     wait     ;fosc/4/256/256 (tak jest ustawiony tmr0) dla 4Mhz 64 ms dla 8 Mhz 32 ms
 		;dwa bity nizsze musza byc zaznaczone przez okolo 64ms
 
@@ -340,7 +292,7 @@ lcd_init_3_petla
 	bcf	INTCON,T0IF
          call     wait
          
-         decfsz   tmp,f
+         decfsz   n_lcd,f
          goto     lcd_init_3_petla
 
          IF (polozenie_danych_lcd == 0) 
